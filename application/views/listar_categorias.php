eeafe2d1e4be5b2b1737f43afd54b5761d927a3a
*Cleudirdiaz* <cleudir.diaz17@gmail.com>
2021-03-19 01:57:38 -0300

    Adicionando uma nova view novo gasto

---
+++ application/views/listar_categorias.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Gastos Básicos do mês  </h1>
+    
+    <table>
+        <tr>
+            <td> Mês </td> <td> Luz</td> <td> Água </td> <td> Alimentação</td>
+        </tr>
+
+        <tr>
+            <td> Janeiro </td> <td> R$105,00 reiais </td> <td> R$59,00 reais </td> <td> R$700,00 reias </td>
+        </tr>
+
+        <tr>
+            <td> Fevereiro </td> <td>R$110,00 reiais</td> <td> R$70,00 reais </td> <td> R$805,00 reiais</td>
+        </tr>
+
+        <tr>
+            <td> Março </td> <td> R$120,00 reiais</td> <td> R$81,00 reais </td> <td> R$855,00 reiais</td>
+        </tr>
+        </table>
+
+        <form action="">
+        <label for="nome">Nome:<input type="text" name="nome"></label></br>
+        <label for="descricao">Descrição:<input type="text" name="descrição"></label></br>
+
+    </form>
+</body>
+</html>
---
+++ application/views/listar_gastos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Veja aqui os gastos do seu auxilio estudantil no mês de Março </h1>
+    
+    <table>
+        <tr>
+            <td> Descrição do Produto </td> <td> Data da Compra</td> <td> Valor da Compra </td> <td> Tipo de Auxilio</td>
+        </tr>
+
+        <tr>
+            <td> Dispositivo Eletronico </td> <td> 05/03/2021</td> <td> R$1,600 reais </td> <td> Auxulio emergencial</td>
+        </tr>
+
+        <tr>
+            <td> Refrigerante + 2 Coxinhas (casadinha) </td> <td> 06/03/2021</td> <td> R$5,00 reais </td> <td> Auxulio Estudantil</td>
+        </tr>
+
+        <tr>
+            <td> Bannner de Projeto </td> <td> 14/03/2021</td> <td> R$100,00 reais </td> <td> Edital de Pesquisa destinado ao apoio a Pesquisa e Extensão</td>
+        </tr>
+
+    </table>
+
+</body>
+</html>
